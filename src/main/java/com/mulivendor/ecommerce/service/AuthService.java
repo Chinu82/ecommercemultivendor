212a952d314d753905cd3e09367e53ed8fd7ec71
Chinmaya Sahoo <cinshu39@gmail.com>
2025-07-07 00:46:32 +0530

    feat : adding otp concepts and login

--- src/main/java/com/mulivendor/ecommerce/service/AuthService.java
+++ src/main/java/com/mulivendor/ecommerce/service/AuthService.java
@@ -1,7 +1,15 @@
 package com.mulivendor.ecommerce.service;
 
+import com.mulivendor.ecommerce.request.LoginRequest;
 import com.mulivendor.ecommerce.request.SignupRequest;
+import com.mulivendor.ecommerce.response.AuthResponse;
 
 public interface AuthService {
-    String createUser(SignupRequest req);
+
+    void sentLoginSignupOtp(String email) throws Exception;
+    String createUser(SignupRequest req) throws Exception;
+    AuthResponse signing(LoginRequest request);
 }
+//Before there is method for createUser which is working fine
+//For signup and login otp : 04:47:10
+//For signing : 05:10:00
